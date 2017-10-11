0277524a21541825a5530a9c76617bf3ecd88551
Eduardo Salvador <eduardo.salvador@rakuten.com>
2017-10-11 16:03:10 +0100

    code for 2nd chapter

---
+++ src/thedevaspect/Model.fs
@@ -0,0 +1,32 @@
+﻿type Details= {
+    Name:  string
+    Description: string}
+
+type Item ={
+    Details: Details}
+
+type Exit= 
+    | PassableExist of Details * destination: Room
+    | LockedExist of Details  * key: Item * next: Exit
+    | NoExist of Details option
+
+and Exits= {
+    North:  Exit
+    South:  Exit
+    West:  Exit
+    East:  Exit}
+
+and Room ={
+    Details: Details
+    Items: Item list
+    Exists: Exits}
+
+let firstRoom =
+    {Details = { Name= "First Room"; Description="You're stangding on a room'"};
+    Items = [];
+    Exists = 
+        {
+        North=  NoExist(None);
+        South=  NoExist(None);
+        West=  NoExist(None);
+        East=  NoExist(None)}}
